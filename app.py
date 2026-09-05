d18f46131750b172404bbd27fbf45f6d238279da
RushikeshThube2831 <rushikeshthube890@gmail.com>
2026-09-05 07:06:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,10 +75,15 @@ def ai_request(instruction):
 # =========================================================
 
 def get_db():
-
-    conn = sqlite3.connect(DATABASE)
+    conn = sqlite3.connect(
+        DATABASE,
+        timeout=10
+    )
 
     conn.row_factory = sqlite3.Row
+
+    conn.execute("PRAGMA busy_timeout = 10000")
+    conn.execute("PRAGMA journal_mode = WAL")
 
     return conn
 
